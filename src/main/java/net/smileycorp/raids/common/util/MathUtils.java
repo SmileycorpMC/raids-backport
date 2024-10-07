63bc2987bb3e673cba3244a4baf27df19d0b8718
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-07 21:06:49 +0100

    add better item throwing ai

--- src/main/java/net/smileycorp/raids/common/util/MathUtils.java
+++ src/main/java/net/smileycorp/raids/common/util/MathUtils.java
@@ -1,5 +1,10 @@
 package net.smileycorp.raids.common.util;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
+
 public class MathUtils {
     
     public static float lerp(float delta, float start, float end) {
@@ -20,6 +25,18 @@ public class MathUtils {
     
     public static float clamp(float val, float min, float max) {
         return val < min ? min : val > max ? max : val;
+    }
+    
+    public static void throwItem(EntityLivingBase entity, ItemStack stack, Vec3d target) {
+        EntityItem item = new EntityItem(entity.world, entity.posX, entity.posY + entity.getEyeHeight() - 0.3, entity.posZ, stack);
+        item.setThrower(entity.getUniqueID().toString());
+        Vec3d vel = target.subtract(item.posX, item.posY, item.posZ)
+                .normalize().scale(0.3);
+        item.motionX = vel.x;
+        item.motionY = vel.y;
+        item.motionZ = vel.z;
+        item.setDefaultPickupDelay();
+        entity.world.spawnEntity(item);
     }
     
 }
