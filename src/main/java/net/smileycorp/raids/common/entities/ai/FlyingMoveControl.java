819728b1e8bd073e77a7b80af8e1eea5b10909c1
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-14 22:07:06 +0100

    make allay move control based on fly speed attribute

--- src/main/java/net/smileycorp/raids/common/entities/ai/FlyingMoveControl.java
+++ src/main/java/net/smileycorp/raids/common/entities/ai/FlyingMoveControl.java
@@ -1,6 +1,7 @@
 package net.smileycorp.raids.common.entities.ai;
 
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityMoveHelper;
 import net.minecraft.util.math.MathHelper;
 
@@ -31,9 +32,10 @@ public class FlyingMoveControl extends EntityMoveHelper {
             entity.motionY *= 0.5;
             entity.motionZ *= 0.5;
         } else {
-            entity.motionX += d0 / d3 * 0.05 * speed;
-            entity.motionY += d1 / d3 * 0.05 * speed;
-            entity.motionZ += d2 / d3 * 0.05 * speed;
+            double speed = this.speed * entity.getEntityAttribute(SharedMonsterAttributes.FLYING_SPEED).getAttributeValue();
+            entity.motionX += d0 / d3 * speed;
+            entity.motionY += d1 / d3 * speed;
+            entity.motionZ += d2 / d3 * speed;
             if (entity.getAttackTarget() == null) entity.rotationYaw = -((float)MathHelper.atan2(entity.motionX, entity.motionZ)) * 57.295776F;
             else {
                 double d4 = entity.getAttackTarget().posX - entity.posX;
