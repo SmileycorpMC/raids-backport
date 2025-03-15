c86bc119dee19ad5898aded92addbc58fbc3cfe6
Smileycorp <callum.j.longbottom@gmail.com>
2025-03-15 21:48:57 +0000

    add check to make sure futuremc gui is enabled before running custom rendering

--- src/main/java/net/smileycorp/raids/integration/futuremc/FutureMCClientIntegration.java
+++ src/main/java/net/smileycorp/raids/integration/futuremc/FutureMCClientIntegration.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemStack;
 import thedarkcolour.futuremc.client.gui.GuiVillager;
+import thedarkcolour.futuremc.config.FConfig;
 
 import java.util.AbstractMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ public class FutureMCClientIntegration {
     private static Map.Entry<ItemStack, Integer> CACHE = null;
     
     public static boolean shouldRenderDiscounts(ItemStack stack) {
+        if (!FConfig.INSTANCE.getVillageAndPillage().newVillagerGui) return false;
         Minecraft mc = Minecraft.getMinecraft();
         GuiScreen screen = mc.currentScreen;
         boolean flag = screen instanceof GuiVillager && CACHE != null && CACHE.getKey() == stack && CACHE.getValue() > 0 && stack.getCount() != CACHE.getValue();
@@ -21,6 +23,7 @@ public class FutureMCClientIntegration {
     }
     
     public static void setCachedStack(ItemStack stack, int discount) {
+        if (!FConfig.INSTANCE.getVillageAndPillage().newVillagerGui) return;
         CACHE = new AbstractMap.SimpleEntry<>(stack, discount);
     }
     
