8eeb37b5bde8b1921f4771bb7dc56f31ae67a2d8
Smileycorp <callum.j.longbottom@gmail.com>
2025-08-11 21:43:50 +0100

    bump verison

--- src/main/java/net/smileycorp/raids/common/Constants.java
+++ src/main/java/net/smileycorp/raids/common/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
 	
 	public static final String NAME = "Raids";
 	public static final String MODID = "raids";
-	public static final String VERSION = "1.1.4b";
+	public static final String VERSION = "1.1.4c";
 	public static final String DEPENDENCIES = "after:crossbows;after:futuremc;required-after:atlaslib@1.1.7";
 	public static final String PATH = "net.smileycorp.raids.";
 	public static final String CLIENT = PATH + "client.ClientProxy";
