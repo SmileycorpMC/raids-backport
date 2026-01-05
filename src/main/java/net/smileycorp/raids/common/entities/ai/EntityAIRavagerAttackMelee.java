e53c9607d394e779bbbb84c76551e0649019edd8
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-01-05 13:33:07 +0000

    clean code

--- src/main/java/net/smileycorp/raids/common/entities/ai/EntityAIRavagerAttackMelee.java
+++ src/main/java/net/smileycorp/raids/common/entities/ai/EntityAIRavagerAttackMelee.java
@@ -7,13 +7,13 @@ import net.minecraft.entity.ai.EntityAIAttackMelee;
 public class EntityAIRavagerAttackMelee extends EntityAIAttackMelee {
     
     public EntityAIRavagerAttackMelee(EntityCreature entity) {
-        super(entity, 1.0D, true);
+        super(entity, 1, true);
     }
     
     @Override
     protected double getAttackReachSqr(EntityLivingBase target) {
-        float distance = attacker.width - 0.1F;
-        return distance * 2.0F * distance * 2.0F + target.width;
+        float distance = attacker.width - 0.1f;
+        return distance * 2f * distance * 2f + target.width;
     }
     
 }
