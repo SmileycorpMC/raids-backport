9e949581e392d7efbf5bb96d84910f6b0c13a918
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-04-04 00:05:23 +0100

    rename version

--- src/main/java/net/smileycorp/raids/common/Constants.java
+++ src/main/java/net/smileycorp/raids/common/Constants.java
@@ -16,8 +16,8 @@ public class Constants {
 	
 	public static final String NAME = "Raids";
 	public static final String MODID = "raids";
-	public static final String VERSION = "1.1.5";
-	public static final String DEPENDENCIES = "after:crossbows;after:futuremc;required-after:atlaslib@[1.1.9,)";
+	public static final String VERSION = "1.1.5a";
+	public static final String DEPENDENCIES = "after:crossbows;after:futuremc;required-after:atlaslib@[1.1.10,)";
 	public static final String PATH = "net.smileycorp.raids.";
 	public static final String CLIENT = PATH + "client.ClientProxy";
 	public static final String SERVER = PATH + "common.CommonProxy";
